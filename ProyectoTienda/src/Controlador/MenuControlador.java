682a6f7f3bf4ce10f1b42ce3078777046f39dc22
SrVictor <victoris666@gmail.com>
2016-05-25 19:27:34 +0200

    Prueba 4

--- ProyectoTienda/src/Controlador/MenuControlador.java
+++ ProyectoTienda/src/Controlador/MenuControlador.java
@@ -26,7 +26,6 @@ public class MenuControlador implements ActionListener {
             SeleccionArticulo sac = new SeleccionArticulo();
             vista.cerrarVentana();
             sac.getSeleccion();
-            System.out.println("A");
         } else if (evento.getActionCommand().equals(MenuPrincipal.BAJAPARTICULO)) {
         } else if (evento.getActionCommand().equals(MenuPrincipal.MODIFICARARTICULO)) {
         } else if (evento.getActionCommand().equals(MenuPrincipal.IMPORTARARTICULOS)) {
